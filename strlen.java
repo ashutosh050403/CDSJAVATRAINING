3bc1dee14cd961d1033e41d252ee3fa2f2310957
ashutosh050403 <ashu.maur678678@gmail.com>
2024-07-19 16:10:41 +0530

    adding string length program

---
+++ strlen.java
@@ -0,0 +1,13 @@
+public class strlen {
+    public static void main(String[] args) {
+        String name="ashutosh";
+        int count=0;
+       
+       for(char c: name.toCharArray()){
+            count++;
+           
+        }
+        System.out.print(count);
+    }
+    
+}
