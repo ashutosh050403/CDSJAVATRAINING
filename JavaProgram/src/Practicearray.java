41f93d50dab2884b700ceb083ad018618d826564
ashutosh050403 <ashu.maur678678@gmail.com>
2024-07-25 11:21:44 +0530

    array practice

---
+++ JavaProgram/src/Practicearray.java
@@ -0,0 +1,15 @@
+
+
+public class Practicearray {
+    public static void main(String[] args) {
+        String[] friendsName = {"anuj","aditya","abhay","adarsh","ankit"};
+        for(int i=0;i<friendsName.length;i++){
+            System.out.println(friendsName[i]);
+        }
+        for (String s : friendsName) {
+            System.out.println(s);
+        }
+
+
+    }
+}
